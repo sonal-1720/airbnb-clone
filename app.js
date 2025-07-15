0312aecb7337504c9ec78098243ddd488c6be7d9
Sonal Kawade <kawadesonal9@gmail.com>
2025-07-15 14:22:02 +0530

    Add root route to handle /

--- app.js
+++ app.js
@@ -117,6 +117,11 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings"); // or render a home page if you prefer
+});
+
+
 // app.get('/', (req,res)=>{
 //     res.send('Hello');
 // });
